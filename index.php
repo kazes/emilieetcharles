87a56c85290600010a75cf1d79421d4c1dda9f95
kazes <supakazes@hotmail.com>
2014-02-09 19:31:42 +0100

    sass and header added

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
+
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/base.css" type="text/css">
 
     <title>Emilie & Charles</title>
@@ -15,18 +18,34 @@
 
 <body>
     <div id="content">
+        <div id="header">
+            <div class="page-width">
+                <ul id="nav">
+                    <li id="nav-1"><a href="#content">Le mariage</a></li>
+                    <li id="nav-2"><a href="#content">La demande</a></li>
+                    <li id="nav-3"><a href="#content">S'organiser</a></li>
+                    <li id="nav-4"><a href="#content" title="Retour en haut de page">Retour en haut de page</a></li>
+                    <li id="nav-5"><a href="#content">Partager</a></li>
+                    <li id="nav-6"><a href="#content">Liste de mariage</a></li>
+                    <li id="nav-7"><a href="#content">Contact</a></li>
+                </ul>
+            </div>
+        </div>
 
-        <?php include('inc/header.inc.php'); ?>
+
+        <div class="page-width">
+            <?php for ($i = 1; $i <= 100; $i++) { ?>
+                page-width <?php echo $i; ?><br/>
+            <?php } ?>
+        </div>
+
+        <div class="page-width" id="footer">
+
+            hello footer
+
+        </div>
         
-        <div id="page">
-
-
-
-        </div><!-- #page -->
-        
-        <?php include('inc/footer.inc.php'); ?>
-        
-    </div><!-- #content -->
+    </div>
 
 
 
