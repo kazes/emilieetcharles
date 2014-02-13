eff0cb7970fb9833641fbbc21b825c7b623b5fe1
kazes <supakazes@hotmail.com>
2014-02-14 00:18:11 +0100

    add layer 1 and 2

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8">
 
     <!-- GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,600|Merriweather:400,300,700' rel='stylesheet' type='text/css'>
+
+
+
     <link rel="stylesheet" href="css/base.css" type="text/css">
 
     <title>Emilie & Charles</title>
@@ -32,19 +35,43 @@
             </div>
         </div>
 
+        <div class="page-content">
+            <div id="cover" class="page-width">
 
-        <div class="page-width">
+                <img src="img/data/cover.png" class="img-100 ma db" alt="Emilie et Charles, le 2 Mai 2014 à Bénodet"/>
+                <p class="font-15 color-marroon-dark tac" style="padding: 35px 0 75px 0">
+                    Émilie + Charles = <3 : C’est la seule équation que Charles ait jamais réussi à résoudre… et encore il a mis 8 ans !
+                </p>
+            </div>
+
+            <div class="bg-marroon">
+                <div class="page-width " id="yes">
+                    <h2 class="font-josefin-semibold font-28 ttu">émilie a dit oui !</h2>
+                    <p>
+                        Emilie a dit « oui » ! Enfin, plutôt « vâng »,
+                        parce que c’était sur la Baie d’Ha Long l’été dernier,
+                        un bel endroit pour se jeter à l’eau, non ?
+                        Depuis, nous n’avons pas touché terre.
+                        Nous n’accosterons que le 2 mai prochain à Bénodet.
+                        <br/>
+                        <br/>
+                        Nous comptons sur vous pour fêter ça !
+                    </p>
+                </div>
+            </div>
+
+
             <?php for ($i = 1; $i <= 100; $i++) { ?>
                 page-width <?php echo $i; ?><br/>
             <?php } ?>
+
+
+            <div class="page-width" id="footer">
+
+                hello footer
+
+            </div>
         </div>
-
-        <div class="page-width" id="footer">
-
-            hello footer
-
-        </div>
-        
     </div>
 
 
